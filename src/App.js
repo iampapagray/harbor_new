56ac25195123bca6df3bb6e0ab845f80c8c28644
MaghnusM <me@maghn.us>
2022-05-30 16:11:38 -0400

    new home merge

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import './styles/App.css';
 import Leena from './components/Leena.js';
 import Loyalty from './components/Loyalty.js';
-import MiniGame from './components/MiniGame.js';
+import Home from './components/Home.js';
 import {
   BrowserRouter,
   Routes,
@@ -17,25 +17,11 @@ function App() {
       <BrowserRouter>
         <NavBar style="light"/>
         <Routes>
-          <Route path="/" element={ <MiniGame /> } />
+          <Route path="/" element={ <Home /> } />
           <Route path="/leena" element={ <div><Leena /><Footer /></div> } />
           <Route path="/loyalty" element={ <div><Loyalty /><Footer /></div> } />
         </Routes>
       </BrowserRouter>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,107 @@
+import MiniGame from './MiniGame.js';
+import { Button } from 'react-bootstrap';
+import '../styles/Home.css';
+
+import GameInHands from '../assets/home/hands.png';
+import HarborMobileSample from '../assets/home/mobile-prev.png';
+import HarborTimelineIcon from '../assets/home/harbortimelineicon.png';
+import ManAndBoat from '../assets/home/boat-and-man.png';
+import leena from '../assets/home/leena.png';
+import loyalty from '../assets/home/loyalty.png';
+import ArrowLeft from '../assets/home/arrow-left.svg';
+import ArrowRight from '../assets/home/arrow-right.svg';
+import Team from '../assets/home/team.png';
+
+const Home = () => {
+
+    return (
+        <div>
+            <MiniGame />
+            <div className="roadmap-container">
+                <div className="roadmap-box roadmap-box-1">
+                    <div className="roadmap-box-1-top">
+                        <p className="roadmap-title">Overview</p>
+                    </div>
+                    <div className="roadmap-box-1-bottom">
+                        <div className="roadmap-box-1-bottom-left">
+                            <img src={GameInHands} />
+                            <p className="desc roadmap-box-1-bottom-left-text">Onboarding the next 100m blockchain gamers</p>
+                        </div>
+                        <div className="roadmap-box-1-bottom-right">
+                            <p className="desc roadmap-box-1-bottom-right-text">Crafting top mobile gaming experiences we all cherish</p>
+                            <img src={HarborMobileSample} />
+                        </div>
+                    </div>
+                </div>
+                <div className="roadmap-box roadmap-box-2">
+                    <div className="roadmap-box-2-left">
+                        <p className="roadmap-title roadmap-title-box-2">Vision</p>
+                        <div className="roadmap-box-2-timeline">
+                            <div className="roadmap-box-2-timeline-line-item">
+                                <div className="roadmap-box-2-timeline-line-item-left">
+                                    <img src={HarborTimelineIcon} />
+                                </div>
+                                <div className="roadmap-box-2-timeline-line-item-right">
+                                    <p className="roadmap-box-2-timeline-line-item-right-top">Merging F2P and P2E</p>
+                                    <p className="roadmap-box-2-timeline-line-item-right-bottom">Harbor merges the best from story driven mobile free-to-play and blockchain-based play-and-earn economics.</p>
+                                </div>
+                            </div>
+                            <div className="roadmap-box-2-timeline-line-item">
+                                <div className="roadmap-box-2-timeline-line-item-left">
+                                    <img src={HarborTimelineIcon} />
+                                </div>
+                                <div className="roadmap-box-2-timeline-line-item-right">
+                                    <p className="roadmap-box-2-timeline-line-item-right-top">Making blockchain gaming fun</p>
+                                    <p className="roadmap-box-2-timeline-line-item-right-bottom">Harbor merges the best from story driven mobile free-to-play and blockchain-based play-and-earn economics.</p>
+                                </div>
+                            </div>
+                            <div className="roadmap-box-2-timeline-line-item">
+                                <div className="roadmap-box-2-timeline-line-item-left">
+                                    <img src={HarborTimelineIcon} />
+                                </div>
+                                <div className="roadmap-box-2-timeline-line-item-right">
+                                    <p className="roadmap-box-2-timeline-line-item-right-top">Driving mass adoption</p>
+                                    <p className="roadmap-box-2-timeline-line-item-right-bottom">Harbor merges the best from story driven mobile free-to-play and blockchain-based play-and-earn economics. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <Button className="box-2-play-button" variant="secondary">Explore Harbor</Button>{' '}
+                    </div>
+                    <div className="roadmap-box-2-right">
+                        <img src={ManAndBoat} />
+                    </div>
+                </div>
+                <div className="roadmap-box-3">
+                </div>
+                <div className="roadmap-box roadmap-box-upcoming-games">
+                    <div className="roadmap-box-upcoming-games-top">
+                        <p className="roadmap-title">Upcoming Games</p>
+                        <div className="roadmap-icon-menu">
+                            <img src={ArrowLeft} />
+                            <img src={ArrowRight} />
+                        </div>
+                    </div>
+                    <div className="roadmap-box-upcoming-games-bottom">
+                        <div className="roadmap-box-upcoming-games-bottom-item">
+                            <img src={leena} />
+                            <p className="roadmap-box-upcoming-games-bottom-item-title">Leena's Place</p>
+                            <p className="roadmap-box-upcoming-games-bottom-item-desc">Leena returns home and becomes mayor of her small town with the vision of building her boring town into a modern lively abode for her friends and family</p>
+                            <Button className="roadmap-box-upcoming-games-play-button" variant="secondary">Learn More</Button>{' '}
+                        </div>
+                        <div className="roadmap-box-upcoming-games-bottom-item">
+                            <img src={loyalty} />
+                            <p className="roadmap-box-upcoming-games-bottom-item-title">Loyalty</p>
+                            <p className="roadmap-box-upcoming-games-bottom-item-desc">Kick up your own gang and run your family’s crime activities. Choose which mobsters and gangsters should be devoted to your Cosa Nostra and lead them against the fierce competition of the Mafia Town.</p>
+                            <Button className="roadmap-box-upcoming-games-play-button" variant="secondary">Learn More</Button>{' '}
+                        </div>
+                    </div>
+                </div>
+                <div className="roadmap-box roadmap-box-team">
+                    <img src={Team} />
+                </div>
+            </div>
+        </div>
+    );
+}
+    
+export default Home;
